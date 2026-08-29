561e53501d006a2386e03409beb2d721b2f26ad8
avalon-kontra <avalon.kontra@gmail.com>
2026-08-29 16:20:44 +0500

    Add site files
    Add site files

--- worker-config.js
+++ worker-config.js
@@ -4,7 +4,7 @@ export default {
   compatibility_date: "2025-01-01",
   d1_databases: [
     {
-      binding: "KONTRA_STATUS",
+      binding: "STATUS_DB",
       database_name: "kontra-status-db",
       database_id: "4beccfab-f433-4b41-92b4-fda63ff17e34"
     }
